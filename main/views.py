15aa17512fdd13f8cf04ef844e92bf34e79dcb41
Elmira <elmira.arifzade@gmail.com>
2025-11-29 02:19:54 +0400

    Add all project files

---
+++ main/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+def home(request):
+    return render(request, "/Users/elmira/Desktop/django/firstproject/main/templates/main/book.html")
+# Create your views here.
